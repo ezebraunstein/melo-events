dc36fec915ca6edef3486626910f877d9243c085
Ezequiel Braunstein <ezequiel.braunstein@gmail.com>
2023-09-11 21:50:33 -0300

    :bug:Hamburguer

--- src/components/Hamburguer.jsx
+++ src/components/Hamburguer.jsx
@@ -9,22 +9,24 @@ export default function Hamburger({ isOpen, onClick }) {
 
             <style jsx>{`
                 .hamburger{
-                    width: 4rem;
-                    height: 4rem;
+                    width: 5rem;
+                    height: 5rem;
                     display: flex;
                     justify-content: space-around;
                     flex-flow: column nowrap;
                     z-index: 10;
                     right: 50px;
+                    overflow: hidden;
                 }
 
                 .burger{
-                    width: 4rem;
+                    width: 5rem;
                     height: 0.50rem;
                     border-radius: 10px;
                     background-color: #EAFF1A;
                     transform-origin: 1px;
-                    transition: all 0.3s linear;
+                    transition: all 0.1s linear;
+                    overflow: hidden;
                 }
 
                 .burger1{
